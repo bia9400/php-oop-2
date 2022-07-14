e4359cd48b0c9d746aafd502ae8189998a2456bd
bia9400 <ferro.biagio@gmail.com>
2022-07-14 17:24:30 +0200

    echo of username

--- index.php
+++ index.php
@@ -4,11 +4,20 @@ require_once "classi/User.php";
 require_once "classi/Products.php";
 $user1= new User("Biagio Ferro","ciaopeppe");
 $carta1= new PaymentMethods();
-$carta1->setCardId(234258735623875)->setDataScadenza(2020)->paymentSucsesss();
+$carta1->setCardId(234258735623875)->setDataScadenza(2025)->paymentSucsesss();
 $croccantini= new Food();
+$cuccia=new Toys();
 $croccantini->setPrice(100)->getSconto($user1->status);
+$cuccia->setPrice(200)->getSconto($user1->status);
 if($carta1->canIBuy){
-    var_dump($croccantini);
+    echo "Benvenuto:". " " .  $user1->getUserName() ." <br>";
+    if($user1->status){
+        echo "Hai ottenuto uno sconto perché sei registrato";
+    }
+    else{
+        echo "Prezzo pieno, non sei registrato";
+    }
+    var_dump($croccantini,$cuccia);
 }
 else{
     echo "Carta scaduta non puoi acquistare il prodotto";
